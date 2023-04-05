83c7c912e005516a5d8857459ddfad0c90294c7c
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2023-04-05 23:34:02 +0200

    first routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.kr test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <h2>hello react</h2>
+      <hr />
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <ul className="navbar-nav mr-auto">
+        <li><Link to={'/'} className="nav-link"> Home </Link></li>
+        <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+        <li><Link to={'/about'} className="nav-link">About</Link></li>
+      </ul>
+      </nav>
+      <hr />
+      <Routes>
+          <Route exact path='/' element={Home} />
+          <Route path='/contact' element={Contact} />
+          <Route path='/about' element={About} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
